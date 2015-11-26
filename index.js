8b4328bcb127df9e66d18f4afa5e5ae8765dff01
piccagliani <piccagliani.dev@gmail.com>
2015-11-26 16:20:06 +0900

    fix

--- index.js
+++ index.js
@@ -2,54 +2,64 @@ var es = require('event-stream');
 var gutil = require('gulp-util');
 var path = require('path');
 var ect = require('ect');
+var async = require('async');
 
 module.exports = function (opt) {
 
-  opt = opt || {};
-  if (!opt.ext) opt.ext = '.ect';
-  if (!opt.data) opt.data = {};
-  if (!opt.outExt) opt.outExt = '.html';
+    opt = opt || {};
+    if (!opt.ext) opt.ext = '.ect';
+    if (!opt.data) opt.data = {};
 
-  return es.map(function (file, callback) {
-    //function for compile locals async
+    if (!opt.outExt) opt.outExt = '.html';
     var dataCallback = typeof(opt.data) == 'function' ? opt.data : function (file, cb) {
-      
-      process.nextTick(function () {
-        cb(opt.data);
-      });
+        process.nextTick(function () {
+            cb(opt.data);
+        });
     };
 
-    //compile locals async
-    try {
-      var filePath = file.base;
-      var fileCwd = file.cwd;
-      var fileName = gutil.replaceExtension(path.basename(file.path), "");
+    return es.map(function (file, callback) {
+        //function for compile locals async
 
-      var relativeBase = path.relative(fileCwd, filePath);
-      var relativePath = path.join(relativeBase, fileName);
-      //relative path for dynamic locals creation
-      dataCallback(relativePath, function (data) {
+
+        //compile locals async
         try {
-          var html = ect({
-            root: filePath,
-            ext: opt.ext
-          });
+            var filePath = file.base;
 
-          html.render(fileName, data, function (error, html) {
-            if(error) gutil.log(gutil.colors.red('Error gulp-ect detail: ' + error));
-            //error && gutil.log(gutil.colors.red('Error gulp-ect: ' + error.message));
-            file.contents = new Buffer(html);
-            file.path = gutil.replaceExtension(file.path, opt.outExt);
-          });
+
+            var relativePath = path.relative(file.base, file.path);
+
+            relativePath = relativePath.replace(new RegExp(""+opt.ext+"$"),"");
+            //relative path for dynamic locals creation
+
+
+
+            dataCallback(relativePath, function (data) {
+
+                try {
+                    var html = ect({
+                        root: filePath,
+                        ext: opt.ext
+                    });
+
+                    html.render(file.path, data, function (error, html) {
+                        if(error)
+                            throw error;
+                        file.contents = new Buffer(html);
+
+                        file.path = gutil.replaceExtension(file.path, opt.outExt);
+
+                    });
+
+                } catch (e) {
+                    gutil.log(gutil.colors.red('Error gulp-ect: ' + e.message));
+                }
+
+                callback(null, file);
+            });
         } catch (e) {
-          gutil.log(gutil.colors.red('Error gulp-ect: ' + e.message));
+            gutil.log(gutil.colors.red('Error Locals gulp-ect: ' + e.message));
         }
-        callback(null, file);
-      });
-    } catch (e) {
-      gutil.log(gutil.colors.red('Error Locals gulp-ect: ' + e.message));
-    }
 
 
-  });
+    });
 };
